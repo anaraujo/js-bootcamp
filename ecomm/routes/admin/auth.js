2b2e226e23cc6bb287da9371e0c772c6265035c0
Ana Laura de Araujo <anaraujo0847@gmail.com>
2020-09-21 17:08:06 -0300

    E-Commerce App: image and file upload

--- ecomm/routes/admin/auth.js
+++ ecomm/routes/admin/auth.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const { check, validationResult } = require('express-validator');
 
+const { handleErrors } = require('./middlewares');
 const usersRepo = require('../../repositories/users');
 const signupTemplate = require('../../views/admin/auth/signup');
 const signinTemplate = require('../../views/admin/auth/signin');
@@ -11,6 +11,7 @@ const {
 	requireEmailExists,
 	requireValidPasswordForUser
 } = require('./validators');
+const signup = require('../../views/admin/auth/signup');
 
 const router = express.Router();
 
@@ -18,20 +19,19 @@ router.get('/signup', (req, res) => {
 	res.send(signupTemplate({ req }));
 });
 
-router.post('/signup', [ requireEmail, requirePassword, requirePasswordConfirmation ], async (req, res) => {
-	const errors = validationResult(req);
+router.post(
+	'/signup',
+	[ requireEmail, requirePassword, requirePasswordConfirmation ],
+	handleErrors(signupTemplate),
+	async (req, res) => {
+		const { email, password } = req.body;
+		const user = await usersRepo.create({ email, password });
 
-	if (!errors.isEmpty()) {
-		return res.send(signupTemplate({ req, errors }));
+		req.session.userId = user.id;
+
+		res.redirect('/admin/products');
 	}
-
-	const { email, password, passwordConfirmation } = req.body;
-	const user = await usersRepo.create({ email, password });
-
-	req.session.userId = user.id;
-
-	res.send('Account created!!!');
-});
+);
 
 router.get('/signout', (req, res) => {
 	req.session = null;
@@ -42,20 +42,19 @@ router.get('/signin', (req, res) => {
 	res.send(signinTemplate({}));
 });
 
-router.post('/signin', [ requireEmailExists, requireValidPasswordForUser ], async (req, res) => {
-	const errors = validationResult(req);
+router.post(
+	'/signin',
+	[ requireEmailExists, requireValidPasswordForUser ],
+	handleErrors(signupTemplate),
+	async (req, res) => {
+		const { email } = req.body;
 
-	if (!errors.isEmpty()) {
-		return res.send(signinTemplate({ errors }));
+		const user = await usersRepo.getOneBy({ email });
+
+		req.session.userId = user.id;
+
+		res.redirect('/admin/products');
 	}
-
-	const { email } = req.body;
-
-	const user = await usersRepo.getOneBy({ email });
-
-	req.session.userId = user.id;
-
-	res.send('You are signed in!!!');
-});
+);
 
 module.exports = router;
